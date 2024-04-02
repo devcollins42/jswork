7b01caa368755df584c086ba4f4f85e77e2b8d85
devcollins42 <nnadicollins33@gmail.com>
2024-04-02 02:26:01 +0100

    get.js

--- get.js
+++ get.js
@@ -8,18 +8,18 @@ mySubmit.onclick = function(){
     age = Number(age);
 
     if (age >= 100){
-        resultElement.textContent =`You're too Old to enter this site`;
+        resultElement.textContent =`You're TOO OLD to enter this site`;
     }
     else if(age == 0){
         resultElement.textContent =`You can't enter. c'mon, you were just born.`;
     }
     else if (age >= 18){
-        resultElement.textContent = `you are old enough to enter this site`;
+        resultElement.textContent = `You are old enough to enter this site`;
     }
     else if(age<0){
         resultElement.textContent = `Your age can't be below 0`;
     }
     else{
-        resultElement.textContent = `Your age must be 18+ to eneter this site`;
+        resultElement.textContent = `You must must be 18+ to eneter this site`;
     }
 }
